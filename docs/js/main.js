d08dcd2cad891b060decd07f8613218462b21bed
Silvia <silviayehyeh26@gmail.com>
2026-07-12 18:52:08 +0800

    add menu button

--- docs/js/main.js
+++ docs/js/main.js
@@ -1,7 +1,29 @@
-const startButton = document.getElementById("startButton");
+const startButton = document.getElementById("startAR");
 
 startButton.addEventListener("click", () => {
 
-    alert("準備開始探索！");
+ alert("準備開始探索！");
+ window.location.href = "npc.html";
+});
+
+const startButton = document.getElementById("startAR");
+
+startButton.addEventListener("click", () => {
+
+ alert("準備開始探索！");
+ window.location.href = "npc.html";
+
+});
+
+
+
+const menuBtn = document.getElementById("menuBtn");
+
+const menuPanel = document.getElementById("menuPanel");
+
+
+menuBtn.addEventListener("click",()=>{
+
+menuPanel.classList.toggle("active");
 
 });
